7f83a346708ab85db40cdc8f07a4d575e4ae6a50
frbennett <31456183+frbennett@users.noreply.github.com>
2017-10-25 09:20:50 +1000

    Minor changes

--- progress_bar.py
+++ progress_bar.py
@@ -4,12 +4,14 @@ from tkinter import *
 
 class interval_bar(Canvas):
     def __init__(self, parent):
-        Canvas.__init__(self, parent, width=1000)
+        Canvas.__init__(self, parent, height =30)
         self.num_rect = 6
         self.score = 0
         self.rectangle = []
+        height = 10
+        width = 50
         for i in range(self.num_rect):
-            self.rectangle.append(self.create_rectangle(50 * i, 50, 50 + 50 * i, 30 + 50, fill="steel blue", tag='BOX'))
+            self.rectangle.append(self.create_rectangle(width * i, 10, width + width * i, height + 10, fill="steel blue", outline="steel blue", tag='BOX'))
 
     def update_score(self):
         if self.score == self.num_rect:
